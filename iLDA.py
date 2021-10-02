f052105f83609c2d1ec119e9cc85f7acce25b35f
Travis <tleedepriest@gmail.com>
2021-10-02 17:56:38 -0400

    changed iLDA class to be an object that accepts an initial or see model and a number of keyword arguments depending on the number of levels you want in the hierarchy

--- iLDA.py
+++ iLDA.py
@@ -11,47 +11,42 @@ print(common_texts)
 common_dictionary = Dictionary(common_texts)
 common_corpus = [common_dictionary.doc2bow(text) for text in common_texts]
 
-class iLDA(LdaModel):
+class iLDA():
+    """
+    This class will use LdaModel in a recursive fashion. Using the first
+    instance of the model to train models based on each cluster in the first.
+    """
+    def __init__(self, first_trained_model, **kwargs):
 
-    def __init__(self, corpus=None, num_topics=100, id2word=None,
-            distributed=False, chunksize=2000, passes=1, update_every=1,
-            alpha='symmetric', eta=None, decay=0.5, offset=1.0, eval_every=10,
-            iterations=50, gamma_threshold=0.001, minimum_probability=0.01,
-            random_state=None, ns_conf=None, minimum_phi_value=0.01,
-            per_word_topics=False, callbacks=None, dtype=np.float32, 
-            hierarchy_levels=3, level_two_max_topics=5, 
-            level_three_max_topics=5):
-        
-        super().__init__(corpus, num_topics, id2word,
-            distributed, chunksize, passes, update_every,
-            alpha, eta, decay, offset, eval_every,
-            iterations, gamma_threshold, minimum_probability,
-            random_state, ns_conf, minimum_phi_value,
-            per_word_topics, callbacks, dtype)
+        self.__dict__.update(kwargs)
+        self.first_trained_model = first_trained_model
+        self.addtional_splits = len(kwargs.items())
 
-        # number of levels to the hierarchy.
-        self.hierarchy_levels = hierarchy_levels
-
-        # max level of topics you want each cluster in
-        # first hierarhcy to be split into. Minimum is 2.
-        self.level_two_range = level_two_max_topics
-        self.level_three_range = level_three_max_topics
+def test():
+    lda = iLDA(common_corpus, num_topics=i)
+    topics = lda.get_topics()
     
-lda = iLDA(common_corpus, num_topics=10)
-topics = lda.get_topics()
+    #distribution of words over topics
+    #Gives you the word weights for each topic.
+    all_word_weights = []
+    for num, topic in enumerate(topics):
+        print(f"{num} topic")
+        word_weight_topic_vector = []
+        for word_ind, weight in enumerate(topic):
+            word = common_dictionary[word_ind]
+            word_weight_topic_vector.append((word, weight))
+            word_weight_topic_vector.sort(key=lambda x:x[1], reverse=True)
+        all_word_weights.append(word_weight_topic_vector)
+        #distribution of topics over documents
+        #gives you the topic weights for the first document.
+    print([(all_word_weights[x], y) for x, y in lda[common_corpus[0]]])
 
-# distribution of words over topics
-# Gives you the word weights for each topic.
-all_word_weights = []
-for num, topic in enumerate(topics):
-    print(f"{num} topic")
-    word_weight_topic_vector = []
-    for word_ind, weight in enumerate(topic):
-        word = common_dictionary[word_ind]
-        word_weight_topic_vector.append((word, weight))
-        word_weight_topic_vector.sort(key=lambda x:x[1], reverse=True)
-    all_word_weights.append(word_weight_topic_vector)
+def main():
+    lda = LdaModel(common_corpus, num_topics=5)
+    kwargs = {
+            "level_one_max_splits": 5,
+            "level_two_max_splits": 5}
+    ilda = iLDA(lda, **kwargs)
 
-# distribution of topics over documents
-# gives you the topic weights for the first document.
-print([(all_word_weights[x], y) for x, y in lda[common_corpus[0]]])
+if __name__ == "__main__":
+    main()
