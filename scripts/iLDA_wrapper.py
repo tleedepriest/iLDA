bade6d73d2d479692dbb3e81b5c048c241102037
Travis <tleedepriest@gmail.com>
2022-01-17 18:01:31 -0500

    I added a format_topics_sentences function that creates a dataframe of that tags each document with the dominant topic for a given model. This DataFrame will be used to train a new set of models, one for each dominant topic

--- scripts/iLDA_wrapper.py
+++ scripts/iLDA_wrapper.py
@@ -6,8 +6,11 @@ import re
 import numpy as np
 from pathlib import Path
 import matplotlib.pyplot as plt
+import pandas as pd
+
 from gensim.models.ldamodel import LdaModel
-from gensim.test.utils import common_texts
+from gensim.test.utils import common_texts, datapath
+
 from gensim.corpora.dictionary import Dictionary
 from gensim.models.coherencemodel import CoherenceModel
 from gensim.parsing.preprocessing import split_on_space, preprocess_string, strip_tags, strip_punctuation, strip_multiple_whitespaces, strip_short, remove_stopwords
@@ -62,14 +65,13 @@ class iLDA(LdaModel):
         self.tokens = tokens
 
         if model_eval_info is None:
-            self.model_eval_info = {
-                    "level_one":
-                    {
-                        "models":[],
+            sub_dict = {"models":[],
                         "num_topics_coherence":[],
-                        "optimal_model": None
-                }
-                    }
+                        "optimal_model": None}
+
+            self.model_eval_info = {"level_one": sub_dict,
+                                    "level_two": sub_dict,
+                                    "level_three": sub_dict}
 
     def get_num_topics(self, level):
         num_tops_cos = self.model_eval_info[level]["num_topics_coherence"]
@@ -80,10 +82,17 @@ class iLDA(LdaModel):
         return [num_top_co[1] for num_top_co in num_tops_cos]
 
     def find_optimal_model(self, level):
+        """
+        Returns The optimal model based on the elbow of the coherence
+        vs. the num_topics curve. Sets optimal_model key in mode_eval_info
+        to the optimal model found.
+        """
         models = self.model_eval_info[level]["models"]
         (optimal_topics,
          optimal_model_index) = self.find_optimal_topics(level)
-        return models[optimal_model_index]
+        optimal_model = models[optimal_model_index]
+        self.model_eval_info[level]["optimal_model"] = optimal_model
+        return optimal_model
 
     def find_optimal_topics(self, level):
         """
@@ -147,11 +156,15 @@ class iLDA(LdaModel):
 
     def train_models(self, level):
         """
-        Instantiates the same model through a range of the number of topics.
-        Returns a list of LDAmodels
+        trains models. sets models key value to list of models.
         """
-        for topic_num in range(2,
-                self.seed_model_max_topics+1, self.seed_model_step):
+        if level == "level_one":
+            topic_nums = [x for x in
+                          range(2,
+                                self.seed_model_max_topics+1,
+                                self.seed_model_step)]
+
+        for topic_num in topic_nums:
 
             model = LdaModel(self.corpus, topic_num, self.id2word,
                     self.distributed, self.chunksize, self.passes,
@@ -201,14 +214,41 @@ def get_doc_paths(docs_dir_path):
     return [x for x in Path(
         docs_dir_path).glob("**/*") if x.is_file()]
 
+def format_topics_sentences(ldamodel, corpus, texts, doc_paths):
+    # Init output
+    sent_topics_df = pd.DataFrame()
+
+    # Get main topic in each document
+    for i, row in enumerate(ldamodel[corpus]):
+        row = sorted(row, key=lambda x: (x[1]), reverse=True)
+        # Get the Dominant topic, Perc Contribution and Keywords for each document
+        for j, (topic_num, prop_topic) in enumerate(row):
+            if j == 0:  # => dominant topic
+                wp = ldamodel.show_topic(topic_num)
+                topic_keywords = ", ".join([word for word, prop in wp])
+                sent_topics_df = sent_topics_df.append(pd.Series([int(topic_num), round(prop_topic,4), topic_keywords]), ignore_index=True)
+            else:
+                break
+    sent_topics_df.columns = ['Dominant_Topic', 'Perc_Contribution', 'Topic_Keywords']
+
+    # Add original text to the end of the output
+    contents = pd.Series(texts)
+    doc_paths = pd.Series(doc_paths)
+    sent_topics_df = pd.concat(
+        [doc_paths, sent_topics_df, contents], axis=1)
+    return sent_topics_df
+
+
 def main():
     # first retrieve text, tokens, dictionary, and corpus
     texts_tokens = []
+    raw_texts = []
     custom_filters = [lambda x: x.lower(), strip_tags, strip_punctuation, strip_multiple_whitespaces, remove_stopwords, strip_short]
     docs_dir_path = "20news_home"
     doc_paths = get_doc_paths(docs_dir_path)
     for doc_path in doc_paths:
         raw_text = get_raw_text(doc_path)
+        raw_texts.append(raw_text)
         raw_text = remove_emails(raw_text)
         raw_text = remove_in_article(raw_text)
         raw_text = remove_names(raw_text)
@@ -218,12 +258,11 @@ def main():
 
     bigrams = Phrases(texts_tokens)
     texts_tokens = [bigrams[tokens] for tokens in texts_tokens]
-    print(texts_tokens)
     dct = Dictionary(texts_tokens)
     corpus = [dct.doc2bow(text) for text in texts_tokens]
 
     kwargs = {
-            "seed_model_max_topics":10,
+            "seed_model_max_topics":40,
             "seed_model_step":2,
             "level_one_max_splits": 5,
             "level_two_max_splits": 5
@@ -237,7 +276,24 @@ def main():
     ilda.train_models(level="level_one")
     ilda.train_coherence(level="level_one")
     ilda.vis_topics_coherences(level="level_one")
-    print(ilda.find_optimal_model(level="level_one"))
+    optimal_model = ilda.find_optimal_model(level="level_one")
+    temp_file = datapath("optimal_model")
+    optimal_model.save(temp_file)
+    num_topics = optimal_model.get_topics().shape[0]
+    for top in range(0, num_topics):
+        topic_terms = optimal_model.get_topic_terms(top)
+        topic_inx = [topic_term[0] for topic_term in topic_terms]
+        words = [dct[id_] for id_ in topic_inx]
+        print(words)
+
+    df = format_topics_sentences(optimal_model, corpus, raw_texts, doc_paths)
+    df.to_csv("test.csv")
+    # next step in the pipeline is to slice the df into rows by dominant
+    # topic value and to train submodels for each of the
+    # slices of rows.
+    # for each value in dominant topic, train a submodel that splits the
+    # group into at most five topics.
+
     #print(ilda.get_models_coherence())
     #print(ilda.get_first_set_models())
 
